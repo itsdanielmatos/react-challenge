9fc0dafce5fce1a003ccc822ce381ee43672af20
Daniel Matos <itsdanielmatos@gmail.com>
2018-02-01 18:27:36 +0000

    Search Component Receives Placeholder through props

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,11 +2,12 @@ import React, { PropTypes } from 'react'
 
 import './Search.css'
 
-function Search({searchValue, onChange}) {
-  return (<input className="Search" placeholder="Search Word" value={searchValue} onChange={onChange} />)
+function Search({searchValue, onChange, placeholder = "Search Word"}) {
+  return (<input className="Search" placeholder={placeholder} value={searchValue} onChange={onChange} />)
 }
 
 Search.propTypes = {
+   placeholder: PropTypes.string,
    searchValue: PropTypes.string.isRequired,
    onChange: PropTypes.func.isRequired,
 }
